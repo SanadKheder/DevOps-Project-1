f5549ef275d94c653561749b520399889545294b
SanadKheder <167946262+SanadKheder@users.noreply.github.com>
2026-01-24 21:40:21 +1100

    Updating website

--- Website/main.js
+++
@@ -1,9 +0,0 @@
-
-
-function doMenu() {
-	let menu = document.getElementById("menu");
-	let optionNumber = menu.options.selectedIndex;
-	let url = menu.options[optionNumber].value;
-	location.href = url;
-}
-
